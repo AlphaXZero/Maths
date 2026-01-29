b798173a8f10a7caf4cd0681b2e19ca5a6c0968b
gg <georgevanderveen@yahoo.fr>
2026-01-29 18:08:13 +0100

    WIP : Bugfix exercise 4

--- matrix_inversion_exercises/matrix_inversion.py
+++ matrix_inversion_exercises/matrix_inversion.py
@@ -3,9 +3,6 @@ import math
 import numpy as np
 from typing import TypeAlias
 
-global operations
-operations = []
-
 Matrix: TypeAlias = list[list[int]]
 Rowcol: TypeAlias = tuple[int, int]
 
@@ -19,7 +16,7 @@ def invert_row(matrix: Matrix, row1: int, row2: int) -> Matrix:
     return matrix
 
 
-def substract_row(matrix: Matrix, row1: int, row2: int, k: int) -> Matrix:
+def subtract_row(matrix: Matrix, row1: int, row2: int, k: int) -> Matrix:
     matrix[row1] = [i - j * k for i, j in zip(matrix[row1], matrix[row2])]
     return matrix
 
@@ -36,7 +33,7 @@ def determinant_gaussian(matrix: Matrix) -> float:
             return 0
         for j in range(i + 1, len(matrix)):
             k = matrix[j][i] / matrix[i][i]
-            matrix = substract_row(matrix, j, i, k)
+            matrix = subtract_row(matrix, j, i, k)
     return math.prod([matrix[i][i] for i in range(len(matrix))])
 
 
@@ -46,51 +43,57 @@ def identity_matrix(matrix: Matrix) -> Matrix:
     ]
 
 
-def gauss_jordan_lower_triangular(matrix: Matrix, output: Matrix) -> tuple[Matrix]:
+def gauss_jordan_lower_triangular(matrix: Matrix, inv_matrix: Matrix) -> tuple[Matrix]:
     """
     Take the max value in each row's first column, then switch rows if the max is not in the first row (to avoid division by zero).
     Divide the first row to get a 1 in the first column, then subtract this row from every other row with a coefficient if needed.
     Repeat, starting with the second row and second column, and so on.
+    return a tuple with the matrix that will became the unit matrix and the inverted_matrix
     """
     for actual_index in range(len(matrix) - 1):
-        line_where_max = [
+        pivot_row = [
             i
             for i in matrix
             if i[actual_index] == max([i[actual_index] for i in matrix])
         ][0]
-        if matrix.index(line_where_max) != actual_index:
-            matrix = invert_row(matrix, matrix.index(line_where_max), actual_index)
-            output = invert_row(output, matrix.index(line_where_max), actual_index)
-        output = divide_row(output, actual_index, line_where_max[actual_index])
-        matrix = divide_row(matrix, actual_index, line_where_max[actual_index])
+        if matrix.index(pivot_row) != actual_index:
+            inv_matrix = invert_row(inv_matrix, matrix.index(pivot_row), actual_index)
+            matrix = invert_row(matrix, matrix.index(pivot_row), actual_index)
+        inv_matrix = divide_row(inv_matrix, actual_index, pivot_row[actual_index])
+        matrix = divide_row(matrix, actual_index, pivot_row[actual_index])
         for i in range(actual_index + 1, len(matrix)):
-            output = substract_row(output, i, actual_index, matrix[i][actual_index])
-            matrix = substract_row(matrix, i, actual_index, matrix[i][actual_index])
-    output = divide_row(output, -1, matrix[-1][-1] if matrix[-1][-1] != 0 else 1)
-    matrix = divide_row(matrix, -1, matrix[-1][-1] if matrix[-1][-1] != 0 else 1)
-    return matrix, output
+            inv_matrix = subtract_row(
+                inv_matrix, i, actual_index, matrix[i][actual_index]
+            )
+            matrix = subtract_row(matrix, i, actual_index, matrix[i][actual_index])
+    inv_matrix = divide_row(inv_matrix, -1, matrix[-1][-1])
+    matrix = divide_row(matrix, -1, matrix[-1][-1])
+    return matrix, inv_matrix
 
 
-def gauss_jordan_upper_triangular(matrix: Matrix, output: Matrix) -> Matrix:
+def gauss_jordan_upper_triangular(matrix: Matrix, inv_matrix: Matrix) -> Matrix:
     """
     Start from the last column of the penultimate row (because it is the value we want to eliminate),
     then eliminate that value using the row corresponding to the current column.
+    return a tuple with the matrix that became the unit matrix and the inverted_matrix
     """
     for i in range(len(matrix) - 2, -1, -1):
         for j in range(i + 1, len(matrix)):
             if matrix[i][j] != 0:
-                output = substract_row(output, i, j, matrix[i][j])
-                matrix = substract_row(matrix, i, j, matrix[i][j])
-    return matrix, output
+                inv_matrix = subtract_row(inv_matrix, i, j, matrix[i][j])
+                matrix = subtract_row(matrix, i, j, matrix[i][j])
+    return matrix, inv_matrix
 
 
 def gauss_jordan_inversion_matrix(matrix: Matrix) -> Matrix:
-    output = identity_matrix(matrix)
-    matrix, output = gauss_jordan_upper_triangular(
-        *gauss_jordan_lower_triangular(matrix, output)
+    if determinant_gaussian(matrix) == 0:
+        print("Matrix can't be inverted because the determinant is null")
+        return [[]]
+    inv_matrix = identity_matrix(matrix)
+    matrix, inv_matrix = gauss_jordan_upper_triangular(
+        *gauss_jordan_lower_triangular(matrix, inv_matrix)
     )
-    printm(matrix)
-    return [[round(x, 2) for x in row] for row in output]
+    return [[round(x, 2) for x in row] for row in inv_matrix]
 
 
 def printm(matrix: Matrix) -> None:
@@ -108,7 +111,7 @@ def test_ex1():
 def test_ex2():
     A = create_random_matrix((4, 4))
     print("random matrix", A)
-    A = substract_row(A, 0, 1, 2)
+    A = subtract_row(A, 0, 1, 2)
     print("maatrix with row 1 - (2 * row 2)", A)
 
 
