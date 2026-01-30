31c3de418f90ae0cf3b85951821e0fc8f3b73bcb
gg <georgevanderveen@yahoo.fr>
2026-01-30 18:40:44 +0100

    Implemented matrix inversion exercise 5

--- matrix_inversion_exercises/matrix_inversion.py
+++ matrix_inversion_exercises/matrix_inversion.py
@@ -11,7 +11,7 @@ def create_random_matrix(rowcol: Rowcol) -> Matrix:
     return [[random.randint(0, 10) for _ in range(rowcol[1])] for _ in range(rowcol[0])]
 
 
-def invert_row(matrix: Matrix, row1: int, row2: int) -> Matrix:
+def swap_row(matrix: Matrix, row1: int, row2: int) -> Matrix:
     matrix[row1], matrix[row2] = matrix[row2], matrix[row1]
     return matrix
 
@@ -26,7 +26,7 @@ def divide_row(matrix: Matrix, row: int, k: int) -> Matrix:
     return matrix
 
 
-def determinant_gaussian(matrix: Matrix) -> float:
+def get_determinant_by_gauss(matrix: Matrix) -> float:
     # TODO déterminatn quand 0 ??
     for i, row in enumerate(matrix):
         if row[i] == 0:
@@ -37,15 +37,15 @@ def determinant_gaussian(matrix: Matrix) -> float:
     return math.prod([matrix[i][i] for i in range(len(matrix))])
 
 
-def identity_matrix(matrix: Matrix) -> Matrix:
+def get_identity_matrix(matrix: Matrix) -> Matrix:
     return [
         [1 if i == j else 0 for j in range(len(row))] for i, row in enumerate(matrix)
     ]
 
 
-def gauss_jordan_lower_triangular(matrix: Matrix, inv_matrix: Matrix) -> tuple[Matrix]:
+def do_gauss_forward_elimination(matrix: Matrix, inv_matrix: Matrix) -> tuple[Matrix]:
     """
-    Take the max value in each row's first column, then switch rows if the max is not in the first row (to avoid division by zero).
+    Take the max value in each row's first column, then swap rows if the max is not in the first row (to avoid division by zero).
     Divide the first row to get a 1 in the first column, then subtract this row from every other row with a coefficient if needed.
     Repeat, starting with the second row and second column, and so on.
     return a tuple with the matrix that will became the unit matrix and the inverted_matrix
@@ -57,8 +57,8 @@ def gauss_jordan_lower_triangular(matrix: Matrix, inv_matrix: Matrix) -> tuple[M
             if i[actual_index] == max([i[actual_index] for i in matrix])
         ][0]
         if matrix.index(pivot_row) != actual_index:
-            inv_matrix = invert_row(inv_matrix, matrix.index(pivot_row), actual_index)
-            matrix = invert_row(matrix, matrix.index(pivot_row), actual_index)
+            inv_matrix = swap_row(inv_matrix, matrix.index(pivot_row), actual_index)
+            matrix = swap_row(matrix, matrix.index(pivot_row), actual_index)
         inv_matrix = divide_row(inv_matrix, actual_index, pivot_row[actual_index])
         matrix = divide_row(matrix, actual_index, pivot_row[actual_index])
         for i in range(actual_index + 1, len(matrix)):
@@ -71,7 +71,7 @@ def gauss_jordan_lower_triangular(matrix: Matrix, inv_matrix: Matrix) -> tuple[M
     return matrix, inv_matrix
 
 
-def gauss_jordan_upper_triangular(matrix: Matrix, inv_matrix: Matrix) -> Matrix:
+def do_gauss_backward_elimination(matrix: Matrix, inv_matrix: Matrix) -> Matrix:
     """
     Start from the last column of the penultimate row (because it is the value we want to eliminate),
     then eliminate that value using the row corresponding to the current column.
@@ -85,15 +85,58 @@ def gauss_jordan_upper_triangular(matrix: Matrix, inv_matrix: Matrix) -> Matrix:
     return matrix, inv_matrix
 
 
-def gauss_jordan_inversion_matrix(matrix: Matrix) -> Matrix | None:
-    if determinant_gaussian([row[:] for row in matrix]) == 0:
-        print("Matrix can't be inverted because the determinant is null")
+def do_gauss_jordan_inversion(matrix: Matrix) -> Matrix | None:
+    if get_determinant_by_gauss([row[:] for row in matrix]) == 0:
+        print("Matrix can't be inverted because the determinant is zero")
         return [[]]
-    inv_matrix = identity_matrix(matrix)
-    matrix, inv_matrix = gauss_jordan_upper_triangular(
-        *gauss_jordan_lower_triangular(matrix, inv_matrix)
+    inv_matrix = get_identity_matrix(matrix)
+    matrix, inv_matrix = do_gauss_backward_elimination(
+        *do_gauss_forward_elimination(matrix, inv_matrix)
     )
     return [[round(x, 2) for x in row] for row in inv_matrix]
+
+
+def format_equation_solution(reverted_matrix: Matrix, solution_matrix: Matrix) -> None:
+    for i, row in enumerate(reverted_matrix):
+        print(row, end="")
+        print(solution_matrix[i])
+
+
+def system_of_equations_solver() -> Matrix:
+    matrix, solutions = ask_system_of_equations()
+    if get_determinant_by_gauss([row[:] for row in matrix]) == 0:
+        print("Matrix can't be inverted because the determinant is zero")
+    matrix, solutions = do_gauss_backward_elimination(
+        *do_gauss_forward_elimination(matrix, solutions)
+    )
+    return format_equation_solution(
+        matrix, [[round(x, 2) for x in row] for row in solutions]
+    )
+
+
+def ask_system_of_equations() -> Matrix:
+    nbr_unknows = int(input("How many unknows ? "))
+    nbr_equations = int(input("How many equations ? "))
+    if nbr_unknows != nbr_equations:
+        print(
+            "Cannot solve the system using Gauss: number of equations must equal number of unknowns."
+        )
+        return None
+    unknow_list = [[0 for _ in range(nbr_unknows)] for row in range(nbr_unknows)]
+    solutions_list = [[0] for _ in range(nbr_equations)]
+    for i in range(nbr_unknows):
+        for j in range(nbr_unknows + 1):
+            while True:
+                try:
+                    nbr = int(input(f"Enter the value for m{i + 1}{j + 1}: "))
+                    if j < nbr_unknows:
+                        unknow_list[i][j] = int(nbr)
+                    else:
+                        solutions_list[i][0] = int(nbr)
+                    break
+                except ValueError:
+                    print("incorect value, please retry")
+    return unknow_list, solutions_list
 
 
 def printm(matrix: Matrix) -> None:
@@ -104,7 +147,7 @@ def printm(matrix: Matrix) -> None:
 def test_ex1():
     A = create_random_matrix((4, 4))
     print("random matrix", A)
-    A = invert_row(A, 0, 1)
+    A = swap_row(A, 0, 1)
     print("matrix with row 1 and 2 inverted", A)
 
 
@@ -119,23 +162,21 @@ def test_ex3():
     A = create_random_matrix((4, 4))
     print("random matrix", A)
     print("w/ np", np.linalg.det(A))
-    print("w/o np", determinant_gaussian(A))
+    print("w/o np", get_determinant_by_gauss(A))
 
 
 def test_ex4():
     wiki_matrix = [[2, -1, 0], [-1, 2, -1], [0, -1, 2]]
     course_matrix = [[1, 1, 2], [1, 2, 1], [2, 1, 1]]
     zero_det_matrix = [[1, 3, 3, 1], [1, 4, 3, 0], [1, 3, 4, 2], [2, 1, 0, 1]]
-    txt = ["matrix" + i for i in ["from wiki", "from course", "with zero determinant"]]
-    for i in zip(
-        [f"{i} before inversion:" for i in txt],
-        [wiki_matrix, course_matrix, zero_det_matrix],
-        [f"{i}, after inversion:" for i in txt],
-    ):
-        print(i[0])
-        printm(i[1])
-        print(i[2])
-        printm(gauss_jordan_inversion_matrix(i[1]))
+    labels = [
+        "matrix " + i for i in ["from wiki", "from course", "with zero determinant"]
+    ]
+    for label, matrix in zip(labels, [wiki_matrix, course_matrix, zero_det_matrix]):
+        print(f"{label} before inversion: ")
+        printm(matrix)
+        print(f"{label} after inversion:")
+        printm(do_gauss_jordan_inversion(matrix))
         print("-----------------------------")
 
 
@@ -143,4 +184,5 @@ if __name__ == "__main__":
     # test_ex1()
     # test_ex2()
     # test_ex3()
-    test_ex4()
+    # test_ex4()
+    print(system_of_equations_solver())
