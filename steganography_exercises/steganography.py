9b4a5f52f96097a41855c43eb2a99ae5ab798af5
gg <georgevanderveen@yahoo.fr>
2026-02-23 15:32:10 +0100

    Implemented STEGANOGRAPHY* exercise 2 + smalls bugfix

--- steganography_exercises/steganography.py
+++ steganography_exercises/steganography.py
@@ -34,9 +34,34 @@ def hide_image(main_image, secret_image, output_image):
     Image.fromarray(main_pixels).save(output_image)
 
 
+def extract_image(stego_image, extracted_image):
+    """
+    Extracts the hidden image from stego_image
+    and saves it to extracted_image
+    """
+    stego_image = Image.open(stego_image)
+
+    stego_pixels = np.array(stego_image)
+    y, x = stego_image.size
+    for i in range(x):
+        for j in range(y):
+            stego_pixels[i][j] = (
+                (stego_pixels[i, j][0] % 16) * 16,
+                (stego_pixels[i, j][1] % 16) * 16,
+                (stego_pixels[i, j][2] % 16) * 16,
+                stego_pixels[i, j][3],
+            )
+
+    Image.fromarray(stego_pixels).save(extracted_image)
+
+
 if __name__ == "__main__":
     hide_image(
         BASE_DIR / "images/main_image.png",
         BASE_DIR / "images/secret_image.png",
         BASE_DIR / "output/coded_image.png",
+    )
+
+    extract_image(
+        BASE_DIR / "output/coded_image.png", BASE_DIR / "output/decoded_image.png"
     )
